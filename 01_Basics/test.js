036c9f545571a1377fbf7dea7e9449c9870c2e43
Sandeep Dhama <143204720+sandeepdhama022@users.noreply.github.com>
2023-10-16 09:47:28 +0000

    practice to deal with objects inside an array

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,4 +1,24 @@
-const name = "Sandeep Dhama";
-const age = 30;
+// const name = "Sandeep Dhama";
+// const age = 30;
 
-console.log(`Hello! My name is ${name} and I'm ${age} years old.`)
+// console.log(`Hello! My name is ${name} and I'm ${age} years old.`)
+
+const myCoding = [
+    {
+        language: 'HTML',
+        extention: 'html'
+    },
+    {
+        language: 'CSS',
+        extention: 'css'
+    },
+    {
+        language: 'JavaScript',
+        extention: 'js'
+    }
+]
+
+myCoding.forEach( (item,index,arr) => {
+    // console.log(arr);
+    console.log(`Extension for ${item.language} file is ${item.extention}`);
+} )
