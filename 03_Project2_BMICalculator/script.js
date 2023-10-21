dcbfbedbfd125bab69ad07ecfb5cc45256ac2e84
Sandeep Dhama <143204720+sandeepdhama022@users.noreply.github.com>
2023-10-21 16:57:52 +0000

    project 2 complete

--- 03_Project2_BMICalculator/script.js
+++ 03_Project2_BMICalculator/script.js
@@ -7,6 +7,7 @@ form.addEventListener('submit', function(event){
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector('#result')
+    const range = document.querySelector('#range')
 
     console.log(height);
     console.log(weight);
@@ -17,13 +18,22 @@ form.addEventListener('submit', function(event){
     }
     if(isNaN(weight)){
         result.innerHTML = 'Please enter a valid weight'
-        
         return;
     }
 
 
     const bmi = (weight / ((height*height)/10000)).toFixed(2)
+    let rangeResult = "";
+
+    if(bmi < 18.6){
+        rangeResult = 'You are under weight';
+    } else if (bmi >= 18.6 && bmi <= 24.9){
+        rangeResult = 'You are normal';
+    } else if(bmi > 24.9){
+        rangeResult = 'You are over weight'
+    }
 
     result.innerHTML = `BMI is ${bmi}`
+    range.innerHTML = rangeResult
 
 })
