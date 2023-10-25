9380434a0e7d5a69a68bb2e66f7b2fbc6040bf7f
Sandeep Dhama <143204720+sandeepdhama022@users.noreply.github.com>
2023-10-25 08:00:33 +0000

    working project 4

--- 05_Project4_GuessTheNumber/script.js
+++ 05_Project4_GuessTheNumber/script.js
@@ -1,7 +1,22 @@
-const randomNum = Math.random();
-const form = document.querySelector('container')
+const randomNum = Math.floor(Math.random()*100)+1;
+const form = document.querySelector('#container')
+const preGuesses = document.querySelector('#PreviousGuesses')
+const arrPreGuesses = []
+
 
 form.addEventListener('click',function(e){
     e.preventDefault()
-    console.log(Math.round(randomNum*100))
+    console.log(randomNum)
+    preGuesses.innerHTML = randomNum
+    
 })
+
+
+function displayMessage(message){
+    const msgPara = form.appendChild(document.createElement('h3'))
+    msgPara.innerHTML = `<p id="msgParagraph">${message}</p>`
+}
+
+function testNumber(){
+
+}
