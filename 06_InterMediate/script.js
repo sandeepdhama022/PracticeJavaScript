db624bde90a094b2a9bc1a66d300472d91b29737
Sandeep Dhama <143204720+sandeepdhama022@users.noreply.github.com>
2023-10-28 08:37:27 +0000

    practising propagation in event listener

---
+++ 06_InterMediate/script.js
@@ -0,0 +1,10 @@
+const UnList = document.getElementById('uList')
+const element = document.getElementById('car')
+
+UnList.addEventListener('click',function(e){
+    alert('Unordered List Clicked')
+},false)
+
+element.addEventListener('click',function(e){
+    alert('Element was Clicked')
+},false)
