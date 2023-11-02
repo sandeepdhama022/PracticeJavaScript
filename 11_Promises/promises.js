37ae0c99442801b91088401821b216ed6ffab9bb
Sandeep Dhama <143204720+sandeepdhama022@users.noreply.github.com>
2023-11-02 07:09:02 +0000

    practise on promises

--- 11_Promises/promises.js
+++ 11_Promises/promises.js
@@ -1,3 +1,5 @@
+// Creating and Consuming Promises
+// Tareeka Number 1
 const promiseOne = new Promise(function(resolve, reject){
     setTimeout(() => {
         console.log('Task 1 is completed');
@@ -8,3 +10,16 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function(){
     console.log('Executed after the completion of the Task 1');
 })
+
+// Tareeka Number 2
+
+new Promise(function(resolve, reject){
+    setTimeout(() => {
+        console.log('Task 2 is completed');
+        resolve();
+    }, 1000);
+}).then(function(){
+    console.log('Executed after the completion of the Task 2');
+})
+
+// Tareeka Number 3
