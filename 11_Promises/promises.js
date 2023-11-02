d0be760e88db8746ff66a99e71358f809696fb3d
Sandeep Dhama <143204720+sandeepdhama022@users.noreply.github.com>
2023-11-02 08:33:28 +0000

    practising prmises

--- 11_Promises/promises.js
+++ 11_Promises/promises.js
@@ -23,3 +23,39 @@ new Promise(function(resolve, reject){
 })
 
 // Tareeka Number 3
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        console.log('Task 3 is completed');
+        resolve({firstName : 'Sandeep', lastName : 'Dhama'})
+    }, 1000);
+})
+
+promiseThree.then(function(name){
+    console.log(name)
+    return name.firstName
+}).then(function(fName){
+    console.log(fName)
+})
+
+// Tareeka Number 4
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = true
+        if(!error){
+            resolve({username : 'sandeepdhama022', password : '12345'})
+        } else {
+            reject('Something went wrong')
+        }
+    }, 1000);
+})
+
+promiseFour
+.then(function(userData){
+    console.log(userData);
+}).catch(function(err){
+    console.log(err);
+}).finally(function(){
+    console.log('Task 4 Finally Completed');
+})
+
+// Tareeka Number 5
